04d2c9a6031d1e9ba050f3dfe5630c4caa4526fa
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-06-25 23:47:57 +0200

    feat(docker): initialize Docker installation script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,3 +3,17 @@
 set -Eeuo pipefail
 
 echo "[DOCKER] Starting Docker installation..."
+
+info() {
+    echo -e "\e[32m[INFO]\e[0m $1"
+}
+
+warn() {
+    echo -e "\e[33m[WARN]\e[0m $1"
+}
+
+error() {
+    echo -e "\e[31m[ERROR]\e[0m $1"
+}
+
+info "Starting Docker installation..."
