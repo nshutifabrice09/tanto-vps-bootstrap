e7c65c4b0a1f9eeddc73f99cdef9b9b6e0121029
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-07-20 12:11:31 +0200

    currently updating the outdated functions

--- install.sh
+++ install.sh
@@ -1,13 +1,50 @@
-#!/bin/bash
+#!/sur/bin/env bash
 
-set -e
+set -Eeuo pipefail
 
-echo "================================="
-echo "TANTO VPS Bootstrap"
-echo "================================="
+###############################
+# TANTO VPS BOOTSTAP INSTALLER
+###############################
 
-bash scripts/system.sh
-bash scripts/security.sh
-bash scripts/docker.sh
+VERSION="1.0.0"
 
-echo "Bootstrap complete."
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MODULE_DIR="${SCRIPT_DIR}/scripts"
+
+##########
+# LOGGING
+##########
+
+info() {
+    echo -e "\e[32m[INFO]\e[0m $1"
+}
+
+warn() {
+    echo -e "\e[33m[WARN]\e[0m $1"
+}
+
+error() {
+    echo -e "\e[31m[ERROR]\e[0m $1"
+}
+
+#############
+# ROOT CHECK
+#############
+
+require_root() {
+
+    if [[ $EUID -ne 0 ]]; then
+
+        error "This installer must be run as root."
+
+        echo
+        echo "Try:"
+        echo "sudo ./install.sh <option>"
+
+        exit 1
+
+    fi
+
+}
+
+
