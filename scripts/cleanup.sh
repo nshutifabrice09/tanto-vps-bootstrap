b4d397d1377b47d11a0a9cc7e76fa66c60436906
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-08-13 09:26:22 +0200

    refactor(backup and cleanup): use shared common library

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,6 +2,8 @@
 
 set -Eeuo pipefail
 
+source "$(dirname "$0")/../lib/common.sh"
+
 VERSION="1.0.0"
 
 ################
@@ -13,23 +15,6 @@ TEMP_RETENTION_DAYS="${TEMP_RETENTION_DAYS:-3}"
 DOCKER_RETENTION_DAYS="${DOCKER_RETENTION_DAYS:-7}"
 
 DRY_RUN=false
-
-
-##########
-# Logging
-##########
-
-info() {
-    echo -e "\e[32m[INFO]\e[0m $1"
-}
-
-warn() {
-    echo -e "\e[33m[WARN]\e[0m $1"
-}
-
-error() {
-    echo -e "\e[31m[ERROR]\e[0m $1"
-}
 
 #######
 # Help
@@ -115,28 +100,6 @@ parse_arguments() {
     done
 
 }
-
-
-#############
-# Root Check
-#############
-
-require_root() {
-
-    if [[ $EUID -ne 0 ]]; then
-
-        error "This script must be run as root."
-
-        echo
-        echo "Try:"
-        echo "sudo ./cleanup.sh"
-
-        exit 1
-
-    fi
-
-}
-
 
 ##########
 # Helpers
