741628cbd896ebbf83d4edc3c9620ae444face50
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-08-13 14:19:24 +0200

    refactor install.sh

--- install.sh
+++ install.sh
@@ -11,22 +11,6 @@ VERSION="1.0.0"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 MODULE_DIR="${SCRIPT_DIR}/scripts"
 
-##########
-# LOGGING
-##########
-
-info() {
-    echo -e "\e[32m[INFO]\e[0m $1"
-}
-
-warn() {
-    echo -e "\e[33m[WARN]\e[0m $1"
-}
-
-error() {
-    echo -e "\e[31m[ERROR]\e[0m $1"
-}
-
 #############
 # ROOT CHECK
 #############
